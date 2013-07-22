61e7ba90870a26ee1da4b43a3868efc1e1896743
udayakapu <udaya.kapu@gmail.com>
2013-07-22 01:31:38 +0000

    update web.js

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
-var express = require('express');
 var fs = require('fs');
-var app = express();
-app.use(express.logger());
+var express = require('express');
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync('./index.html','utf8');
-  respond.send(buf.toString('utf8',0,buf.length));
-  
-});
+ var buf = fs.readFileSync('./index.html','utf8');
+    response.send(buf.toString('utf8',0,buf.length));
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
